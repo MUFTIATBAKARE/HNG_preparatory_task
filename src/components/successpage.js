8dd17522dc35980874e4a52519f8bee11b0a409a
muftiatbakare <bakare.muftiat@gmail.com>
2022-11-05 22:07:13 +0100

    style: responsive mobile view

---
+++ src/components/successpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css';
+
+
+function successpage() {
+  return <div className='success'>
+    <p>MESSAGE SENT!</p>
+    </div>;
+}
+
+export default successpage;
